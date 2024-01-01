d2e85fc643c76bd4b4fab239675e9ecd11381d97
RajaLairen <75347020+RajaLairen@users.noreply.github.com>
2024-01-01 09:02:35 +0530

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -1,22 +1,5 @@
 const fs = require('fs');
 
-const currentDate = new Date().toUTCString();
-const readmePath = 'README.md';
 
-fs.readFile(readmePath, 'utf-8', (err, data) => {
-  if (err) {
-    console.error('Error reading README.md:', err);
-    process.exit(1);
-  }
-
-  const updatedContent = data.replace(/<!-- last-update -->/,'Hello World');
-  
-  fs.writeFile(readmePath, updatedContent, 'utf-8', (writeErr) => {
-    if (writeErr) {
-      console.error('Error updating README.md:', writeErr);
-      process.exit(1);
-    }
-
-    console.log('README.md updated successfully!');
-  });
-});
+// Write the generated content to the README.md file
+fs.writeFileSync('README.md','# Hello');
